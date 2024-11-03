b5bd6aa943121af0aed4750c584703db792dd26f
pantokrator4 <mralexfour@gmail.com>
2024-11-04 00:25:09 +0400

    gradeBookApp

--- average/getGrade.js
+++ average/getGrade.js
@@ -1,16 +1,36 @@
-function getGrade(score) {
-    if (score >= 100)
-      return 'A++';
-      else if (score >= 90) {
-        return 'A'
-      } else if ( score >=80) {
-        return 'B'
-      } else if (score >=70) {
-        return 'C'
-      } else if ( score >=60) {
-        return 'D'
-      } else return'F'
+function getAverage(scores) {
+    let sum = 0;
+  
+    for (const score of scores) {
+      sum += score;
     }
-  console.log(getGrade(94));
-  console.log(getGrade(82));
-  console.log(getGrade(56));
+  
+    return sum / scores.length;
+  }
+  function getGrade(score) {
+    if (score === 100) {
+      return "A++";
+    } else if (score >= 90) {
+      return "A";
+    } else if (score >= 80) {
+      return "B";
+    } else if (score >= 70) {
+      return "C";
+    } else if (score >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+  
+  function hasPassingGrade(score) {
+    return getGrade(score) !== "F";
+  }
+  function studentMsg(totalScores, studentScore) {
+    if (studentScore > 60) {
+      return 'Class average: '+ getAverage(totalScores) +'. Your grade: '+ getGrade(studentScore) +'. You passed the course.'
+  } else return 'Class average: '+ getAverage(totalScores) +'. Your grade: '+ getGrade(studentScore) +'. You failed the course.'
+  } 
+  
+  console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
+  console.log(studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100));
